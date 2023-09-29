4ded523a9f11ee7a426daf02c2b7f6b764a21b44
humy06001 <humy06001@E-PW0234KS>
2023-09-29 10:11:28 +0200

    Add project files.

---
+++ Kap 3 Uppgift 3.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Uppgift3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Frågar efter ålder
+            Console.WriteLine("Hur gammal är du? Svara med ett heltal.");
+            int age = int.Parse(Console.ReadLine());
+
+            //Jämför ålder
+            if (age < 16)
+            {
+                Console.WriteLine("Du är för ung för att vara med i tävlingen!");
+            }
+            else if (age > 19)
+            {
+                Console.WriteLine("Du är för gammal för att vara med i tävlingen!");
+            }
+            else
+            {
+                Console.WriteLine("Du får vara med i tävlingen!");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
